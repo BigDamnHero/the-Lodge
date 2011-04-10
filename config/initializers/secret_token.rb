12f2e837274de4fe8d23f9135f999f3e9269bb04
Sweet Boy <ken@Inara.(none)>
2011-04-10 17:56:29 -0600

    initial checkin of The Lodge app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheLodge::Application.config.secret_token = '28af5d7fefc69db64043c71b3a24e36ee54eaec618852b898df809a8415202b72456becbd9e4eef8a27408366db69b86dd98e77ed1e3f8ccfa63745442bd6957'
